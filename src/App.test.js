4abc2d9c16daadcacb54299df2b49c0b2c724a90
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-06-10 21:11:37 -0300

    feat: TDD exercise

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import PersonList from './PersonList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+
+  let appWrapper;
+
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+  });
+
+  it('renders a person list', () => {
+    const personList = appWrapper.find(PersonList);
+
+    expect(personList).toHaveLength(1);
+  });
+
+  it('has state', () => {
+    const appState = appWrapper.state();
+ 
+    expect(appState).not.toBeNull();
+  });
+
+  it('has a people property on state', () => {
+    const appState = appWrapper.state();
+
+    expect(appState.people).toBeDefined();
+  });
+
+  it('passes people property of state to personList as prop', () => {
+    const personList = appWrapper.find(PersonList);
+    
+    expect(personList.props().people).toEqual(appWrapper.state().people);
+  });
+
+  
 });
